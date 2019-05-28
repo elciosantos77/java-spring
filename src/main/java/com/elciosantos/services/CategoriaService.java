f3decae7955d4889a248cea7017e3c28c5219d66
Elcio Santos <elcio-santos3@hotmail.com>
2019-05-28 10:53:20 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/elciosantos/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.elciosantos.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elciosantos.domain.Categoria;
+import com.elciosantos.respositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository _repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = _repo.findById(id);
+		return obj.orElse(null);
+	}
+}
